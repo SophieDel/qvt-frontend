e7ccc42d3fb06dc64a1848fbfdb8c1854a8afe19
Guillemette <guillemette.fernier@gmail.com>
2022-10-24 14:42:46 +0200

    front

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Footer.module.css';
+
+function Footer() {
+return (
+    <div>
+    <main className={styles.main}>
+        <div></div>
+        <div>
+            <div>FAQ</div>
+            <div>Nous contacter</div>
+            <div>Mentions légales</div>
+        </div>
+    </main>
+    </div>
+);
+}
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>QVT</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
+      <Footer/>
     </>
   );
 }
