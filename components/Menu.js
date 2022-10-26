24c9b78fe4712f6c665bc0f8ec1ce69246a57393
Guillemette <guillemette.fernier@gmail.com>
2022-10-26 15:42:41 +0200

    aspect menu

--- components/Menu.js
+++ components/Menu.js
@@ -2,6 +2,7 @@ import styles from '../styles/Menu.module.css';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faUser} from "@fortawesome/free-solid-svg-icons"
+import { Button } from 'antd';
 
 
 function Menu() {
@@ -9,22 +10,33 @@ function Menu() {
         <div className={styles.contain}>
             <div>
 <div className={styles.titre}>DASHBOARD
-<div className={styles.soustitre}>Mon dashboard</div>
+<div>
+<Button href = "/dashboard" className={styles.liens}> Mon dashboard</Button></div>
 </div>
 
 <div className={styles.titre}>QUIZ
-<div className={styles.soustitre}>Quiz de la semaine</div>
-<div className={styles.soustitre}>Mon questionnaire</div>
+<div>
+<div>
+<Button href = "/qHebdo" className={styles.liens}> Quiz de la semaine</Button>
+</div>
+<div>
+<Button href = "/questionnaire" className={styles.liens}> Mon questionnaire</Button>
+</div>
+</div>
 </div>
 
 <div className={styles.titre}>REPORTINGS
-    <div className={styles.soustitre}>Mes reportings</div>
+<div>
+<Button href = "/reportings" className={styles.liens}> Mes reportings</Button></div>
 </div>
 </div>
 <div>
 <div className={styles.profil}>
 <FontAwesomeIcon className={styles.icon} icon ={faUser} />
-<div>Voir mon profil</div>
+
+<Button href = "/profil" className={styles.liens}> Mon profil</Button>
+
+
 </div>
 </div>
     </div>
