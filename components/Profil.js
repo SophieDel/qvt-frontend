ee1b0605703ab9f347107ccc37c49cb97bd477bf
Guillemette <guillemette.fernier@gmail.com>
2022-10-27 12:00:38 +0200

    profil

---
+++ components/Profil.js
@@ -0,0 +1,37 @@
+import styles from '../styles/Profil.module.css';
+import Link from 'next/link';
+import Headerblanc from './Headerblanc';
+import Footerblanc from './Footerblanc';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import{login} from '../reducers/user';
+import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import { Modal, Button, Space } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons';
+import Menu from './Menu'
+
+function profil() {
+    
+return (
+    <div className={styles.main}>
+    <Headerblanc />
+<div className={styles.contain}>
+<Menu />
+<div className={styles.profil}>
+    <div className={styles.titre}>Mon profil</div>
+    <div className={styles.infos}></div>
+    <div className={styles.boutons}>
+<Button className={styles.btn} href = "/dashboard">Retour</Button>
+<Button className={styles.btn}>Modifier</Button>
+</div>
+</div>
+</div>
+    <Footerblanc />
+</div>
+);
+}
+
+export default profil;
