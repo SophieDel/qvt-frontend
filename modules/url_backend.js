ac39c4f3e4bd42dfd7d53b299effb3ace6d6d274
SophieDel <sophie.demeulemeester@gmail.com>
2022-11-10 10:26:54 +0100

    [qvt-frontend] change backend url

--- modules/url_backend.js
+++ modules/url_backend.js
@@ -1,4 +1,4 @@
-// const URL_BACKEND = "https://qvt-backend-5kg507ka3-sophiedel.vercel.app"
-const URL_BACKEND = "http://localhost:3000"
+const URL_BACKEND = "https://qvt-backend-7nla6vjfx-sophiedel.vercel.app"
+// const URL_BACKEND = "http://localhost:3000"
 
 module.exports = URL_BACKEND;
